9c5bb92dcf3116d4b4297687169719b9a67e4099
uncle_rat <hyuan95@outlook.com>
2017-10-01 20:41:03 -0500

    Revised saveAsDat method

--- EbayJsonParser.py
+++ EbayJsonParser.py
@@ -2,11 +2,11 @@
 """
 Created on Wed Sep 27 23:15:50 2017
 
-@author: Hao Yuan
+@author: Hao Yuan, Zhaoyin Qin, Yue Sun
 """
 
 """
-FILE: skeleton_parser.py
+REFERENCE FILE: skeleton_parser.py
 ------------------
 Author: Firas Abuzaid (fabuzaid@stanford.edu)
 Author: Perth Charernwattanagul (puch@stanford.edu)
@@ -37,8 +37,8 @@ from re import sub
 columnSeparator = "|"
 
 # Dictionary of months used for date transformation
-MONTHS = dict(Jan='01', Feb='02', Mar='03', Apr='04', May='05', Jun='06',
-              Jul='07', Aug='08', Sep='09', Oct='10', Nov='11', Dec='12')
+MONTHS = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06',
+          'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
 
 """
 Returns true if a file ends in .json
@@ -91,62 +91,121 @@ item in the data set. Your job is to extend this functionality to create all
 of the necessary SQL tables for your database.
 """
 
-
-# Hao
+'''# Yue
 def removeDuplicates(somelist):
     # Your codes below
-    return
+    pre_contain = []
+    for i in range(len(somelist)):
+        if somelist[i][0] in pre_contain:
+            somelist.remove(somelist[i])
+        pre_contain.append(somelist[i][0])
+    return somelist'''
 
 
 # Hao
 def saveAsDat(somelist, filename):
-    # Your codes below
-    return
+    with open(filename, "a") as f:
+        f.writelines("\n".join(somelist))
 
 
 # Yue
-class Item():
-    def __init__(self, item_id, name, seller_id, price, current, first_bid,
-                 num_of_bid, start, end, description):
-        return
-
-def writeItemTable(ItemList):
+class Item:
     # Your codes below
+    def __init__(self, ItemID, Name, Seller_ID, Buy_Price, Currently,
+                 First_Bid, Number_of_Bids, Started, Ends, Description):
+        self.ItemID = ItemID
+        self.Name = Name
+        self.Seller_ID = Seller_ID
+        self.Buy_Price = Buy_Price
+        self.Currently = Currently
+        self.First_Bid = First_Bid
+        self.Number_of_Bids = Number_of_Bids
+        self.Started = Started
+        self.Ends = Ends
+        self.Description = Description
 
-    # Call removeDuplicates, and saveAsDat methods below
-    return
+    def getProperty(self, index):
+        if index == 0:
+            return self.ItemID
+        if index == 1:
+            return self.Name
+        if index == 2:
+            return self.Seller_ID
+        if index == 3:
+            return self.Buy_Price
+        if index == 4:
+            return self.Currently
+        if index == 5:
+            return self.First_Bid
+        if index == 6:
+            return self.Number_of_Bids
+        if index == 7:
+            return self.Started
+        if index == 8:
+            return self.Ends
+        if index == 9:
+            return self.Description
+
 
 
 # Yue
-class Bids():
-    def __init__(self, item_id, bidder_id,time, amount):
-        return
-
-def writeBidTable(BidList):
+def writeItemTable(ItemList, filename):
     # Your codes below
-
+    Item_output = []
+    for i in range(len(ItemList)):
+        element = []
+        for j in range(10):
+            element.append(ItemList[i].getProperty(j) + '|')
+        Item_output.append(element)
     # Call removeDuplicates, and saveAsDat methods below
-    return
+    saveAsDat(Item_output, filename)
+
+
+# Yue
+class Bids:
+    # Your codes below
+    def __init__(self, ItemID, BidderID, Time, Amount):
+        self.ItemID = ItemID
+        self.BidderID = BidderID
+        self.Time = Time
+        self.Amount = Amount
+
+    def getProperty(self, index):
+        if index == 0:
+            return self.BidderID
+        if index == 1:
+            return self.ItemID
+        if index == 2:
+            return self.Time
+        if index == 3:
+            return self.Amount
+
+# Yue
+def writeBidTable(BidList, filename):
+    # Your codes below
+    Item_output = []
+    for i in range(len(BidList)):
+        element = []
+        for j in range(4):
+            element.append(BidList[i].getProperty(j) + '|')
+        Item_output.append(element)
+    # Call removeDuplicates, and saveAsDat methods below
+    saveAsDat(Item_output, filename)
 
 
 # Hao
-def writeCategoryTable(CategoryList):
-    # Your codes below
-    return
+def writeCategoryTable(CategoryList, filename):
+    saveAsDat(CategoryList, filename)
 
 
 # Hao
-def writeItemCatTable(ItemCatList):
-    # Your codes below
-
-    # Call removeDuplicates, and saveAsDat methods below
-    return
+def writeItemCatTable(ItemCatList, filename):
+    saveAsDat(ItemCatList, filename)
 
 
 # Zhaoyin
-class User():
-    '''Represent a user.'''
-
+class User:
+    # Your codes below
     def __init__(self, id, rating):
         self.id = id
         self.rating = rating
@@ -155,18 +214,18 @@ class User():
 
 
 # Zhaoyin
-def writeUserTable(UserList):
+def writeUserTable(UserList, filename):
+    # Your codes below
     user_dat = []
     # Your codes below
     for user in UserList:
         dat = str(user.id) + "|" + str(user.rating) + "|" + user.location + \
               user.country
         user_dat.append(dat)
-    # Call removeDuplicates, and saveAsDat methods below
     # removeDuplicates(user_dat)
-    saveAsDat(user_dat, 'user.dat')
+    saveAsDat(user_dat, filename)
 
-
+# Yue, Hao, Zhaoyin
 def parseJson(json_file):
     ItemList = []
     BidList = []
@@ -183,7 +242,7 @@ def parseJson(json_file):
             item_id = item[u'Item_ID']
             item_name = item[u'Name']
             seller_id = item[u'Seller'][u'UserID']
-            buy_price = transformDollar(item[u'Buy_Price'])
+            #buy_price = transformDollar(item[u'Buy_Price'])
             currently = transformDollar(item[u'Currently'])
             first_bid = transformDollar(item[u'First_Bid'])
             number_of_bids = item[u'Number_of_Bids']
@@ -196,7 +255,7 @@ def parseJson(json_file):
             # Item Object Initialization
             if len(item) == 14:
                 # Item Object with no Buy_Price
-                my_item = Item(item_id, item_name, seller_id, buy_price,
+                my_item = Item(item_id, item_name, seller_id, transformDollar(item[u'Buy_Price']),
                                currently,
                                first_bid, number_of_bids, started, end,
                                description)
@@ -252,13 +311,11 @@ def parseJson(json_file):
 
         # Call writer methods here
         # i.e. writeUserTable(UserList)
-        writeItemTable(ItemList)
-        writeBidTable(BidList)
-        writeCategoryTable(CategoryList)
-        writeItemCatTable(ItemCatList)
-        writeUserTable(UserList)
-
-
+        writeItemTable(ItemList, "ItemTable.dat")
+        writeBidTable(BidList, "BidTable.dat")
+        writeCategoryTable(CategoryList, "CategoryTable.dat")
+        writeItemCatTable(ItemCatList, "ItemCategoryTable.dat")
+        writeUserTable(UserList, "UserTable.dat")
 
 """
 Loops through each json files provided on the command line and passes each file
@@ -279,4 +336,3 @@ def main(argv):
 
 if __name__ == '__main__':
     main(sys.argv)
-
